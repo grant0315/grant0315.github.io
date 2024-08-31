a624e1c5157d249cbf083208e1134aa71e438685
Grant Hopkins <granthopkins0315@gmail.com>
2024-08-31 17:03:29 -0500

    Update About.jsx

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -8,16 +8,16 @@ const About = () => {
       <div className="container">
         <h2 className="about-title">About Me</h2>
         <p className="about-text">
-          Hello! I'm [Your Name], a passionate [Your Profession] with a love for [Your Interests or Specializations].
-          With [X years] of experience in [Your Field/Industry], I've had the privilege of working on a variety of exciting projects,
-          from [mention a couple of notable projects or skills].
+          Hello! I'm Grant Hopkins, a passionate Software Developer with a love for Business Intelligence and Automation.
+          With 3 years of experience in BI & Analytics, I've had the privilege of working on a variety of exciting projects,
+          from NLP, Computer Vision, Gen AI, and Web Development.
         </p>
         <p className="about-text">
-          My journey began in [mention your background or education], and over the years, I've honed my skills in [mention specific skills or technologies].
-          I'm particularly enthusiastic about [mention your passion or what drives you in your profession], and I continuously strive to expand my knowledge and expertise.
+          My journey began in when I was 13, and over the years, I've honed my skills in Python Automation, Web Development, and Data Analytics/Science.
+          I'm particularly enthusiastic about making products that are unique and innovative, and I continuously strive to expand my knowledge and expertise.
         </p>
         <p className="about-text">
-          When I'm not working, you can find me [mention hobbies or interests]. I believe in balancing work with personal growth, and I'm always on the lookout for new challenges.
+          When I'm not working, you can find me working on my peronsal game development projects, and camping. I believe in balancing work with personal growth, and I'm always on the lookout for new challenges.
         </p>
       </div>
     </section>
