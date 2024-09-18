20627e552639d484856bfae449794174d059a9d2
Grant Hopkins <granthopkins0315@gmail.com>
2024-09-18 11:40:57 -0500

    Updated Home, created gradient background, added animation for button, and .intro-title span ("Grant")

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Home.css'; 
+import anime from 'animejs/lib/anime.es.js';
 
 const Home = () => {
+
+    useEffect(() => {
+        // Animation for button
+        const button = document.querySelector('.button-modern');
+        button.addEventListener('mouseenter', () => {
+            anime({
+                targets: '.button-modern',
+                scale: [1, 1.2],
+                duration: 300,
+                easing: 'easeOutInElastic(1, .8)',
+            });
+        });
+
+        button.addEventListener('mouseleave', () => {
+            anime({
+                targets: '.button-modern',
+                scale: [1.2, 1],
+                duration: 500,
+                easing: 'easeOutQuad',
+            });
+        });
+
+        // Span Animation
+        anime({
+            targets: '.intro-title span',
+            opacity: [0, 1],
+            translateY: [-20, 0],
+            delay: 500,
+            duration: 5000,
+            easing: 'easeOutQuad'
+        });
+    }, []);
+
     return (
         <section id="home" className="home-section">
         <div className="intro">
-            <h1 className="intro-title">Hi, I'm Grant Hopkins</h1>
-            <p className="intro-text">
-            I'm a <span className="profession">software engineer and Business Intelligence specialist</span> passionate about create value add software.
-            </p>
-            <p className="intro-text">
-            Welcome to my personal website where you can find more about me and my work. Let's connect!
-            </p>
-            <a href="#contact" className="contact-link">Get in Touch</a>
+            <h1 className="intro-title titillium-web-semibold">Hi, I'm <span className="hero-name">Grant.</span></h1>
+            <h1 className="intro-subtitle titillium-web-semibold">Software Developer</h1>
+            <button className="button-modern titillium-web-regular" href="#About">More About Me</button>
         </div>
         </section>
     )
